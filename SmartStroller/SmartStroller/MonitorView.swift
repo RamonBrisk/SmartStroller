acc5ada827899090aa6457767af3976e6d818fd6
Ramon <l554058776@live.com>
2021-02-07 23:29:23 +0800

    add a monitor view file

---
+++ SmartStroller/SmartStroller/MonitorView.swift
@@ -0,0 +1,20 @@
+//
+//  MonitorView.swift
+//  SmartStroller
+//
+//  Created by 李杨 on 2021/2/7.
+//
+
+import SwiftUI
+
+struct MonitorView: View {
+    var body: some View {
+        Text("sensors' data")
+    }
+}
+
+struct MonitorView_Previews: PreviewProvider {
+    static var previews: some View {
+        MonitorView()
+    }
+}
