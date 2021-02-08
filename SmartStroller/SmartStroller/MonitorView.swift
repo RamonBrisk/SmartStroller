a53760ebb209c4e0c4eb6832b96bc406e8909523
Ramon <l554058776@live.com>
2021-02-08 11:50:12 +0800

    import chart view from https://github.com/AppPear/ChartView

--- SmartStroller/SmartStroller/MonitorView.swift
+++ SmartStroller/SmartStroller/MonitorView.swift
@@ -6,10 +6,18 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct MonitorView: View {
     var body: some View {
-        Text("sensors' data")
+        VStack {
+            MultiLineChartView(data: [([8,32,11,23,40,28], GradientColors.green), ([90,99,78,111,70,60,77], GradientColors.purple), ([34,56,72,38,43,100,50], GradientColors.orngPink)], title: "Title")
+            
+            
+            BarChartView(data: ChartData(values: [("2018 Q4",63150), ("2019 Q1",50900), ("2019 Q2",77550), ("2019 Q3",79600), ("2019 Q4",92550)]), title: "Sales", legend: "Quarterly") // legend is optional
+
+        }
+
     }
 }
 
